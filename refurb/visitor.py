943ab305635c73a7c3584d77bde1ceb1ba17b127
dosisod <39638017+dosisod@users.noreply.github.com>
2022-08-10 16:13:19 -0700

    Dynamically generate visitor methods based:
    This is done with some hacky class-instance manipulation, but I couldnt find
    a better way to add the visitor functions on a per-instance basis (and I didnt
    want to add these to the class itself).

--- refurb/visitor.py
+++ refurb/visitor.py
@@ -4,85 +4,64 @@ from collections import defaultdict
 from inspect import signature
 from typing import Callable, Type
 
-from mypy.nodes import (
-    CallExpr,
-    ForStmt,
-    GeneratorExpr,
-    Node,
-    OpExpr,
-    TryStmt,
-    WithStmt,
-)
+from mypy.nodes import Node
 from mypy.traverser import TraverserVisitor
 
 from . import checks
+from ._visitor_mappings import MAPPINGS
 from .error import Error
+
+Check = Callable[[Node, list[Error]], None]
+
+
+def load_checks(ignore: set[int]) -> defaultdict[Type[Node], list[Check]]:
+    found: defaultdict[Type[Node], list[Check]] = defaultdict(list)
+
+    for info in pkgutil.walk_packages(checks.__path__, "refurb.checks."):
+        if info.ispkg:
+            continue
+
+        module = importlib.import_module(info.name)
+
+        if any(
+            name.startswith("Error")
+            and name != "Error"
+            and getattr(module, name).code in ignore
+            for name in dir(module)
+        ):
+            continue
+
+        if func := getattr(module, "check", None):
+            params = list(signature(func).parameters.values())
+
+            found[params[0].annotation].append(func)
+
+    return found
+
+
+def build_visitor(
+    name: str, ty: Type[Node], checks: defaultdict[Type[Node], list[Check]]
+) -> Callable[["RefurbVisitor", Node], None]:
+    def inner(self: RefurbVisitor, o: Node) -> None:
+        getattr(TraverserVisitor, name)(self, o)
+
+        for check in checks[ty]:
+            check(o, self.errors)
+
+    return inner
 
 
 class RefurbVisitor(TraverserVisitor):
     errors: list[Error]
-    checks: defaultdict[Type[Node], list[Callable[[Node, list[Error]], None]]]
-    ignore: set[int]
 
     def __init__(self, ignore: set[int] | None = None) -> None:
         self.errors = []
-        self.checks = defaultdict(list)
-        self.ignore = ignore or set()
 
-        self.load_checks()
+        checks = load_checks(ignore or set())
+        types = set(checks.keys())
 
-    def load_checks(self) -> None:
-        for info in pkgutil.walk_packages(checks.__path__, "refurb.checks."):
-            if info.ispkg:
-                continue
+        for name, type in MAPPINGS.items():
+            if type in types:
+                func = build_visitor(name, type, checks)
 
-            module = importlib.import_module(info.name)
-
-            if any(
-                name.startswith("Error")
-                and name != "Error"
-                and getattr(module, name).code in self.ignore
-                for name in dir(module)
-            ):
-                continue
-
-            if func := getattr(module, "check", None):
-                params = list(signature(func).parameters.values())
-
-                self.checks[params[0].annotation].append(func)
-
-    def visit_op_expr(self, o: OpExpr) -> None:
-        super().visit_op_expr(o)
-
-        for f in self.checks[OpExpr]:
-            f(o, self.errors)
-
-    def visit_with_stmt(self, o: WithStmt) -> None:
-        super().visit_with_stmt(o)
-
-        for f in self.checks[WithStmt]:
-            f(o, self.errors)
-
-    def visit_call_expr(self, o: CallExpr) -> None:
-        super().visit_call_expr(o)
-
-        for f in self.checks[CallExpr]:
-            f(o, self.errors)
-
-    def visit_try_stmt(self, o: TryStmt) -> None:
-        super().visit_try_stmt(o)
-
-        for f in self.checks[TryStmt]:
-            f(o, self.errors)
-
-    def visit_generator_expr(self, o: GeneratorExpr) -> None:
-        super().visit_generator_expr(o)
-
-        for f in self.checks[GeneratorExpr]:
-            f(o, self.errors)
-
-    def visit_for_stmt(self, o: ForStmt) -> None:
-        super().visit_for_stmt(o)
-
-        for f in self.checks[ForStmt]:
-            f(o, self.errors)
+                setattr(self, name, func.__get__(self))
