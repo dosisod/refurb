e4b8c77e8a6328f7d3d5269da32b83a563de1b26
dosisod <39638017+dosisod@users.noreply.github.com>
2022-09-09 11:44:09 -0700

    Add check for equal comparison chains

---
+++ refurb/checks/logical/use_equal_chain.py
@@ -0,0 +1,55 @@
+from collections import Counter
+from dataclasses import dataclass
+
+from mypy.nodes import ComparisonExpr, Expression, OpExpr
+
+from refurb.checks.common import extract_binary_oper
+from refurb.error import Error
+
+
+@dataclass
+class ErrorUseEqualChain(Error):
+    """
+    When checking that multiple objects are equal to each other, don't use
+    an `and` expression, use a comparison chain instead. For example:
+
+    Bad:
+
+    ```
+    if x == y and x == z:
+        pass
+    ```
+
+    Good:
+
+    ```
+    if x == y == z:
+        pass
+    ```
+
+    Note: if `x` depends on side-effects, then this check should be ignored.
+    """
+
+    code = 124
+    msg: str = "Use `x == y == z` instead of `x == y and x == z`"
+
+
+def has_common_expr(exprs: tuple[Expression, ...]) -> bool:
+    count = Counter(str(x) for x in exprs).most_common(1)[0][1]
+
+    return count > 1
+
+
+def check(node: OpExpr, errors: list[Error]) -> None:
+    exprs = extract_binary_oper("and", node)
+
+    # TODO: remove when next mypy version is released
+    if not exprs:
+        return
+
+    match exprs:
+        case (
+            ComparisonExpr(operators=["=="], operands=[a, b]),
+            ComparisonExpr(operators=["=="], operands=[c, d]),
+        ) if has_common_expr((a, b, c, d)):
+            errors.append(ErrorUseEqualChain(a.line, a.column))
---
+++ test/data/err_124.py
@@ -0,0 +1,17 @@
+x = y = z = 1
+
+# these should match
+
+_ = x == y and x == z
+_ = x == y and y == z
+_ = x == y and z == y
+_ = x == y and x == z and True
+_ = x == y and y == z and z == 1
+
+
+# these should not
+
+_ = x == y and z == 1
+_ = x == y or 1 == z
+_ = x == y or 1 == z
+_ = x == y or x <= z
