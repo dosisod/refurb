fc529aebb210d9f1e766327dbc4c9eb649b90244
dosisod <39638017+dosisod@users.noreply.github.com>
2022-09-15 20:48:29 -0700

    Add "use tuple swap" check for swapping variables

---
+++ refurb/checks/readability/use_tuple_swap.py
@@ -0,0 +1,70 @@
+from dataclasses import dataclass
+
+from mypy.nodes import AssignmentStmt, Block, MypyFile, NameExpr, Statement
+
+from refurb.checks.common import check_block_like
+from refurb.error import Error
+
+
+@dataclass
+class ErrorUseTupleSwap(Error):
+    """
+    You don't need to use a temporary variable to swap 2 variables, you can use
+    tuple unpacking instead:
+
+    Bad:
+
+    ```
+    temp = x
+    x = y
+    y = tmp
+    ```
+
+    Good:
+
+    ```
+    x, y = y, x
+    ```
+    """
+
+    code = 128
+    msg: str = "Use tuple unpacking instead of temporary variables to swap values"  # noqa: E501
+
+
+def check(node: Block | MypyFile, errors: list[Error]) -> None:
+    check_block_like(check_stmts, node, errors)
+
+
+def check_stmts(stmts: list[Statement], errors: list[Error]) -> None:
+    assignments = []
+
+    for stmt in stmts:
+        if isinstance(stmt, AssignmentStmt):
+            assignments.append(stmt)
+
+        else:
+            assignments = []
+
+        if len(assignments) == 3:
+            match assignments:
+                case [
+                    AssignmentStmt(
+                        lvalues=[NameExpr() as a], rvalue=NameExpr() as b
+                    ),
+                    AssignmentStmt(
+                        lvalues=[NameExpr() as c], rvalue=NameExpr() as d
+                    ),
+                    AssignmentStmt(
+                        lvalues=[NameExpr() as e], rvalue=NameExpr() as f
+                    ),
+                ] if (
+                    a.fullname == f.fullname
+                    and b.fullname == c.fullname
+                    and d.fullname == e.fullname
+                ):
+                    errors.append(ErrorUseTupleSwap(a.line, a.column))
+
+                    assignments = []
+
+                case _:
+                    assignments.pop(0)
---
+++ test/data/err_128.py
@@ -0,0 +1,56 @@
+x = 1
+y = 2
+
+# `pass` used as a filler to prevent this check from checking things it shouldnt
+pass
+
+# these will match
+
+tmp = x
+x = y
+y = tmp
+
+pass
+
+filler = 1
+tmp = x
+x = y
+y = tmp
+
+pass
+
+filler = 1
+filler = 2
+tmp = x
+x = y
+y = tmp
+
+pass
+
+tmp = x
+x = y
+y = tmp
+tmp = x
+x = y
+y = tmp
+
+
+# these will not
+
+tmp = x
+y = tmp
+x = y
+
+pass
+
+tmp = x
+x = y
+tmp = 1
+y = tmp
+
+pass
+
+tmp = x
+x = y
+pass
+y = tmp
