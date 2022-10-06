0cb97115c309c452bc8529e4ad6ed92b8a3296c7
dosisod <39638017+dosisod@users.noreply.github.com>
2022-10-05 18:32:44 -0700

    Add "no del" check: (#50)
    This basically checks for instances of the `del` keyword which can be replaced
    with the more descriptive `.pop()` or `.clear()` methods.

---
+++ refurb/checks/builtin/no_del.py
@@ -0,0 +1,69 @@
+from dataclasses import dataclass
+
+from mypy.nodes import DelStmt, IndexExpr, NameExpr, SliceExpr, Var
+
+from refurb.error import Error
+
+
+@dataclass
+class ErrorInfo(Error):
+    """
+    The `del` statement has it's uses, but for the most part, it can be
+    replaced with a more flexible and expressive alternative.
+
+    With `dict` and `list` types you can remove a key/index by using the
+    `.pop()` method. If you want to remove all the elements in a `dict` or
+    `list`, use `.clear()` instead.
+
+    Bad:
+
+    ```
+    names = {"key": "value"}
+    nums = [1, 2, 3]
+
+    del names["key"]
+    del nums[0]
+    del nums[:]
+    ```
+
+    Good:
+
+    ```
+    names = {"key": "value"}
+    nums = [1, 2, 3]
+
+    names.pop("key")
+    nums.pop(0)
+    nums.clear()
+    ```
+    """
+
+    code = 131
+
+
+def check(node: DelStmt, errors: list[Error]) -> None:
+    match node:
+        case DelStmt(
+            expr=IndexExpr(base=NameExpr(node=Var(type=ty)), index=index)
+        ) if str(ty).startswith(("builtins.dict[", "builtins.list[")):
+            match index:
+                case SliceExpr(begin_index=None, end_index=None):
+                    errors.append(
+                        ErrorInfo(
+                            node.line,
+                            node.column,
+                            "Replace `del x[:]` with `x.clear()`",
+                        )
+                    )
+
+                case SliceExpr():
+                    pass
+
+                case _:
+                    errors.append(
+                        ErrorInfo(
+                            node.line,
+                            node.column,
+                            "Replace `del x[y]` with `x.pop(y)`",
+                        )
+                    )
