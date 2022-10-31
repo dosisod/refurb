d1fe4c2424adea93555358e50ffff2359bf28f2c
dosisod <39638017+dosisod@users.noreply.github.com>
2022-10-30 20:04:35 -0700

    Add "no trailing continue" check:
    This is similar to the "no trailing return" check, but it applies to for
    and while loops.

---
+++ refurb/checks/flow/no_trailing_continue.py
@@ -0,0 +1,103 @@
+from dataclasses import dataclass
+from typing import Generator
+
+from mypy.nodes import (
+    Block,
+    ContinueStmt,
+    ForStmt,
+    IfStmt,
+    MatchStmt,
+    Statement,
+    WhileStmt,
+    WithStmt,
+)
+from mypy.patterns import AsPattern
+
+from refurb.error import Error
+
+
+@dataclass
+class ErrorInfo(Error):
+    """
+    Don't explicitly continue if you are already at the end of the control flow
+    for the current for/while loop:
+
+    Bad:
+
+    ```
+    def func():
+        for _ in range(10):
+            print("hello world!")
+
+            continue
+
+    def func2(x):
+        for x in range(10):
+            if x == 1:
+                print("x is 1")
+
+            else:
+                print("x is not 1")
+
+                continue
+    ```
+
+    Good:
+
+    ```
+    def func():
+        for _ in range(10):
+            print("hello world!")
+
+    def func2(x):
+        for x in range(10):
+            if x == 1:
+                print("x is 1")
+
+            else:
+                print("x is not 1")
+    ```
+    """
+
+    code = 133
+    msg: str = "Continue is redundant here"
+
+
+def get_trailing_continue(node: Statement) -> Generator[Statement, None, None]:
+    match node:
+        case ContinueStmt():
+            yield node
+
+        case MatchStmt(bodies=bodies, patterns=patterns):
+            for body, pattern in zip(bodies, patterns):
+                match (body.body, pattern):
+                    case _, AsPattern(pattern=None, name=None):
+                        pass
+
+                    case [ContinueStmt()], _:
+                        continue
+
+                yield from get_trailing_continue(body.body[-1])
+
+        case (
+            IfStmt(else_body=Block(body=[*_, stmt]))
+            | WithStmt(body=Block(body=[*_, stmt]))
+        ):
+            yield from get_trailing_continue(stmt)
+
+    return None
+
+
+def check(node: ForStmt | WhileStmt, errors: list[Error]) -> None:
+    match node:
+        case (
+            ForStmt(body=Block(body=[*prev, stmt]))
+            | WhileStmt(body=Block(body=[*prev, stmt]))
+        ):
+            if not prev and isinstance(stmt, ContinueStmt):
+                return
+
+            for continue_node in get_trailing_continue(stmt):
+                errors.append(
+                    ErrorInfo(continue_node.line, continue_node.column)
+                )
