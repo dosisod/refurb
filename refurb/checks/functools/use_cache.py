a99a61d8c2b6369624da54c7878e5d16c929ba4c
dosisod <39638017+dosisod@users.noreply.github.com>
2022-11-03 15:06:14 -0700

    Add "use @cache decorator" check for Python 3.9 and up:
    See docstring for more info.

---
+++ refurb/checks/functools/use_cache.py
@@ -0,0 +1,55 @@
+from dataclasses import dataclass
+
+from mypy.nodes import ArgKind, CallExpr, Decorator, NameExpr
+
+from refurb.error import Error
+from refurb.settings import Settings
+
+
+@dataclass
+class ErrorInfo(Error):
+    """
+    Python 3.9 introduces the `@cache` decorator which can be used as a
+    short-hand for `@lru_cache(maxsize=None)`.
+
+    Bad:
+
+    ```
+    from functools import lru_cache
+
+    @lru_cache(maxsize=None)
+    def f(x: int) -> int:
+        return x + 1
+    ```
+
+    Good:
+
+    ```
+    from functools import cache
+
+    @cache
+    def f(x: int) -> int:
+        return x + 1
+    ```
+    """
+
+    code = 134
+    msg: str = "Replace `@lru_cache(maxsize=None)` with `@cache`"
+
+
+def check(node: Decorator, errors: list[Error], settings: Settings) -> None:
+    if settings.python_version and settings.python_version < (3, 9):
+        return  # pragma: no cover
+
+    match node:
+        case Decorator(
+            decorators=[
+                CallExpr(
+                    callee=NameExpr(fullname="functools.lru_cache"),
+                    arg_names=["maxsize"],
+                    arg_kinds=[ArgKind.ARG_NAMED],
+                    args=[NameExpr(fullname="builtins.None")],
+                )
+            ]
+        ):
+            errors.append(ErrorInfo(node.line, node.column))
