590ebcc5d4f6c4f33bf74296058fd7ad897509d0
dosisod <39638017+dosisod@users.noreply.github.com>
2022-11-08 16:09:02 -0800

    Bump packages (#105):
    Bump the minimum version for Mypy, as well as some dev packages.
    
    Also bump this version in preparation for the next release.

--- refurb/checks/string/startswith.py
+++ refurb/checks/string/startswith.py
@@ -34,13 +34,7 @@ class ErrorInfo(Error):
 
 
 def check(node: OpExpr, errors: list[Error]) -> None:
-    exprs = extract_binary_oper("or", node)
-
-    # TODO: remove when next mypy version is released
-    if not exprs:
-        return
-
-    match exprs:
+    match extract_binary_oper("or", node):
         case (
             CallExpr(
                 callee=MemberExpr(
