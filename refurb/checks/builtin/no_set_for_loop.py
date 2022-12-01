a3c155aef1a3e38e9d7a8b949ce0444c07635d43
dosisod <39638017+dosisod@users.noreply.github.com>
2022-11-30 19:50:18 -0800

    Add "no set in for loop" check:
    This check only checks for "add" and "discard" being called on a set in a for
    loop, since those are the only functions which have an in-place modifying
    equivalent. The "add" function in a for loop will interfere with the eventual
    "use set comprehension" check, assuming that you are assigning to a newly
    created set. I don't know what the best way to handle this is. The same probably
    applies for calling "append" in a for loop. The best solution in this case is
    to just let 2 errors get emitted, and let the user use their discretion to
    choose what suits their needs best.

---
+++ refurb/checks/builtin/no_set_for_loop.py
@@ -0,0 +1,86 @@
+from dataclasses import dataclass
+
+from mypy.nodes import (
+    Block,
+    CallExpr,
+    ExpressionStmt,
+    ForStmt,
+    MemberExpr,
+    NameExpr,
+    Var,
+)
+
+from refurb.checks.common import unmangle_name
+from refurb.error import Error
+
+
+@dataclass
+class ErrorInfo(Error):
+    """
+    When you want to add/remove a bunch of items to/from a set, don't use a for
+    loop, call the appropriate method on the set itself.
+
+    Bad:
+
+    ```
+    sentence = "hello world"
+    vowels = "aeiou"
+    letters = set(sentence)
+
+    for vowel in vowels:
+        letters.discard(vowel)
+    ```
+
+    Good:
+
+    ```
+    sentence = "hello world"
+    vowels = "aeiou"
+    letters = set(sentence)
+
+    letters.difference_update(vowels)
+    ```
+    """
+
+    code = 142
+    categories = ["builtin"]
+
+
+def check(node: ForStmt, errors: list[Error]) -> None:
+    match node:
+        case ForStmt(
+            index=NameExpr(name=for_name),
+            body=Block(
+                body=[
+                    ExpressionStmt(
+                        expr=CallExpr(
+                            callee=MemberExpr(
+                                expr=NameExpr(node=Var(type=ty)) as set_name,
+                                name=("add" | "discard") as name,
+                            ),
+                            args=[arg],
+                        )
+                    )
+                ]
+            ),
+        ) if str(ty).startswith("builtins.set[") and set_name.name != for_name:
+            new_func = "update" if name == "add" else "difference_update"
+
+            if isinstance(arg, NameExpr):
+                expr = unmangle_name(arg.name)
+                new_expr = "y"
+
+                if unmangle_name(for_name) != expr:
+                    return
+
+            else:
+                expr = "..."
+                new_expr = "... for x in y"
+
+            errors.append(
+                ErrorInfo(
+                    node.line,
+                    node.column,
+                    f"Replace `for x in y: s.{name}({expr})` with `s.{new_func}({new_expr})`",  # noqa: E501
+                )
+            )
