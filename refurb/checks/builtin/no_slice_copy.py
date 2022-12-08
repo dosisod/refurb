cc8e98debbee4935b05f844fe113b9fee3481255
dosisod <39638017+dosisod@users.noreply.github.com>
2022-12-07 20:57:14 -0800

    Add "no slice copy" check (#145)

---
+++ refurb/checks/builtin/no_slice_copy.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+
+from mypy.nodes import AssignmentStmt, DelStmt, IndexExpr, MypyFile, SliceExpr
+from mypy.traverser import TraverserVisitor
+
+from refurb.error import Error
+
+
+@dataclass
+class ErrorInfo(Error):
+    """
+    Don't use a slice expression (with no bounds) to make a copy of something,
+    use the more readable `.copy()` method instead:
+
+    Bad:
+
+    ```
+    nums = [3.1415, 1234]
+    copy = nums[:]
+    ```
+
+    Good:
+
+    ```
+    nums = [3.1415, 1234]
+    copy = nums.copy()
+    ```
+    """
+
+    code = 145
+    msg: str = "Replace `x[:]` with `x.copy()`"
+    categories = ["readability"]
+
+
+class SliceExprVisitor(TraverserVisitor):
+    errors: list[Error]
+
+    def __init__(self, errors: list[Error]) -> None:
+        super().__init__()
+
+        self.errors = errors
+
+    def visit_assignment_stmt(self, node: AssignmentStmt) -> None:
+        node.rvalue.accept(self)
+
+    def visit_del_stmt(self, node: DelStmt) -> None:
+        if not isinstance(node.expr, IndexExpr):
+            node.expr.accept(self)
+
+    def visit_slice_expr(self, node: SliceExpr) -> None:
+        if node.begin_index is node.end_index is node.stride is None:
+            self.errors.append(ErrorInfo(node.line, node.column))
+
+
+def check(node: MypyFile, errors: list[Error]) -> None:
+    node.accept(SliceExprVisitor(errors))
---
+++ test/data/err_145.py
@@ -0,0 +1,17 @@
+# these should match
+
+nums = [1, 2, 3]
+
+_ = nums[:]
+_ = [1, 2, 3][:]
+_ = (1, 2, 3)[:]
+
+
+# these should not
+
+_ = nums.copy()
+_ = nums[1:]
+_ = nums[:1]
+_ = nums[::1]
+
+nums[:] = [4, 5, 6]
