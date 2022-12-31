45deddad0c36f022774d16ece14283ed0d6f633e
dosisod <39638017+dosisod@users.noreply.github.com>
2022-12-30 23:30:04 -0800

    Add ability to parse path expressions resulting from `/` operator

--- refurb/checks/pathlib/util.py
+++ refurb/checks/pathlib/util.py
@@ -1,7 +1,9 @@
-from mypy.nodes import CallExpr, Expression, NameExpr, TypeInfo, Var
+from mypy.nodes import CallExpr, Expression, NameExpr, OpExpr, TypeInfo, Var
 
 
 def is_pathlike(expr: Expression) -> bool:
+    # TODO: just check that the expression is of type `Path` once we actually
+    # get proper type checking
     match expr:
         case CallExpr(
             callee=NameExpr(node=TypeInfo() as ty),
@@ -9,6 +11,9 @@ def is_pathlike(expr: Expression) -> bool:
             return True
 
         case NameExpr(node=Var(type=ty)) if str(ty) == "pathlib.Path":
+            return True
+
+        case OpExpr(left=left, op="/") if is_pathlike(left):
             return True
 
     return False
