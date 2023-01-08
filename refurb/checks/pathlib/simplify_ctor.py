02090a8ab0973001e56a9d37b719afdc7b67cceb
dosisod <39638017+dosisod@users.noreply.github.com>
2023-01-07 20:30:36 -0800

    Add "no explicit cwd" check

---
+++ refurb/checks/pathlib/simplify_ctor.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+
+from mypy.nodes import CallExpr, NameExpr, StrExpr
+
+from refurb.error import Error
+
+
+@dataclass
+class ErrorInfo(Error):
+    """
+    The Path() constructor defaults to the current directory, so don't pass the
+    current directory (".") explicitly.
+
+    Bad:
+
+    ```
+    file = Path(".")
+    ```
+
+    Good:
+
+    ```
+    file = Path()
+    ```
+    """
+
+    code = 153
+    msg: str = 'Replace `Path(".")` with `Path()`'
+    categories = ["pathlib", "readability"]
+
+
+def check(node: CallExpr, errors: list[Error]) -> None:
+    match node:
+        case CallExpr(
+            args=[StrExpr(value=".")],
+            callee=NameExpr(fullname="pathlib.Path"),
+        ):
+            errors.append(ErrorInfo(node.line, node.column))
