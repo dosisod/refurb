8fbf34231de479c6512f6ca8b3c9b61c43dec4e6
doomedraven <doomedraven@users.noreply.github.com>
2023-01-30 20:33:19 +0000

    Add "use pathlib stat" check (#189):
    Hello is my first try to extend refurb. if i did something wrong let me know please. the ID is 154 bcz latest is 153 (according to `grep -r "code = " .| cut -d"=" -f 2|sort|uniq`)

---
+++ refurb/checks/pathlib/getsize.py
@@ -0,0 +1,86 @@
+from dataclasses import dataclass
+
+from mypy.nodes import BytesExpr, CallExpr, MemberExpr, NameExpr, StrExpr, Var
+
+from refurb.checks.pathlib.util import is_pathlike
+from refurb.error import Error
+
+
+@dataclass
+class ErrorInfo(Error):
+    """
+    Don't use the `os.path.getsize` (or similar) functions, use the more modern
+    `pathlib` module instead:
+
+    Bad:
+
+    ```
+    if os.path.getsize("file.txt"):
+        pass
+    ```
+
+    Good:
+
+    ```
+    if Path("file.txt").stat().st_size:
+        pass
+    ```
+    """
+
+    name = "use-pathlib-stat"
+    code = 155
+    categories = ["pathlib"]
+
+
+PATH_TO_PATHLIB_NAMES = {
+    "os.stat": "stat()",
+    "os.path.getsize": "stat().st_size",
+    "os.path.getatime": "stat().st_atime",
+    "os.path.getmtime": "stat().st_mtime",
+    "os.path.getctime": "stat().st_ctime",
+}
+
+
+def check(node: CallExpr, errors: list[Error]) -> None:
+
+    match node:
+        case CallExpr(
+            callee=MemberExpr(fullname=fullname),
+            args=[arg],
+        ):
+            normalized_name = fullname or ""
+
+            for name in ("posix", "nt", "generic"):
+                if normalized_name.startswith(name):
+                    normalized_name = normalized_name.replace(name, "os.")
+
+            new_name = PATH_TO_PATHLIB_NAMES.get(normalized_name)
+
+            if not new_name:
+                return
+
+            if is_pathlike(arg):
+                replace = f"x.{new_name}"
+
+            else:
+                match arg:
+                    case BytesExpr() | StrExpr():
+                        pass
+
+                    case NameExpr(node=Var(type=ty)) if (
+                        str(ty) in ("builtins.str", "builtins.bytes")
+                    ):
+                        pass
+
+                    case _:
+                        return
+
+                replace = f"Path(x).{new_name}"
+
+            errors.append(
+                ErrorInfo(
+                    node.line,
+                    node.column,
+                    f"Replace `{normalized_name}(x)` with `{replace}`",
+                )
+            )
