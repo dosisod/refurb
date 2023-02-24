f131e83f0b0a56ca60c78b1d3cfded3a98ba29b1
dosisod <39638017+dosisod@users.noreply.github.com>
2023-02-23 21:39:39 -0800

    Add `simplify-math-log` check

---
+++ refurb/checks/math/simplify_log.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass
+
+from mypy.nodes import CallExpr, FloatExpr, IntExpr, MemberExpr
+
+from refurb.error import Error
+
+
+@dataclass
+class ErrorInfo(Error):
+    """
+    Use the shorthand `log2` and `log10` functions instead of passing 2 or 10
+    as the second argument to the `log` function. If `math.e` is used as the
+    second argument, just use `math.log(x)` instead, since `e` is the default.
+
+    Bad:
+
+    ```
+    power = math.log(x, 10)
+    ```
+
+    Good:
+
+    ```
+    power = math.log10(x)
+    ```
+    """
+
+    name = "simplify-math-log"
+    code = 163
+    categories = ["math", "readability"]
+
+
+def check(node: CallExpr, errors: list[Error]) -> None:
+    match node:
+        case CallExpr(
+            callee=MemberExpr(fullname="math.log"),
+            args=[_, arg],
+        ):
+            match arg:
+                case IntExpr(value=2 | 10) | FloatExpr(value=2.0 | 10.0):
+                    base = str(arg.value)
+                    new = f"math.log{int(arg.value)}(x)"
+
+                case MemberExpr(fullname="math.e"):
+                    base = "math.e"
+                    new = "math.log(x)"
+
+                case _:
+                    return
+
+            errors.append(
+                ErrorInfo(
+                    node.line,
+                    node.column,
+                    f"Replace `math.log(x, {base})` with `{new}`",
+                )
+            )
---
+++ test/data/err_163.py
@@ -0,0 +1,19 @@
+import math
+
+# these should match
+
+_ = math.log(64, 2)
+_ = math.log(64, 2.0)
+_ = math.log(100, 10)
+_ = math.log(100, 10.0)
+_ = math.log(100, math.e)
+_ = math.log(1 + 2, 2)
+
+
+# these should not
+
+_ = math.log(10, 3)
+_ = math.log(64, 1 + 1)
+
+two = 2
+_ = math.log(10, two)
