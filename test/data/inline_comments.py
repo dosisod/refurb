7e7e1fac237b76b840aa4f5286b602992a24a3a0
dosisod <39638017+dosisod@users.noreply.github.com>
2023-02-28 20:38:51 -0800

    Allow for multiple error codes in `noqa` comments:
    Closes #217.

--- test/data/inline_comments.py
+++ test/data/inline_comments.py
@@ -3,11 +3,22 @@
 x = int(0)  # noqa: FURB123
 x = int(0)  # noqa
 x = int(0) # noqa
+# line below contains trailing whitespace!
+x = int(0) # noqa   
 x = int(0)  # existing comment # noqa
+x = int(0) # noqa: FURB123,RUF100
+x = int(0) # noqa: FURB123, RUF100
+x = int(0) # noqa:  FURB123, RUF100
+x = int(0) # noqa: FURB123 RUF100
+x = int(0) # noqa: FURB123 and RUF100
+x = int(0), int(0)  # noqa: FURB123
 
 # these should not
 
 x = int(0)
 x = int(0)  # some comment
 x = int(0)  # noqa: FURB999
+x = int(0)  # noqa: FURB1234
 x = int(0)  # noqa: 123
+x = str("# noqa: FURB123 ")
+x = str('# noqa: FURB123 ')
