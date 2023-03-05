4db9a438283bd96682e747723e4ee5f226f2aa2d
dosisod <39638017+dosisod@users.noreply.github.com>
2023-03-04 20:50:38 -0800

    Add `no-from-float` check

---
+++ refurb/checks/readability/no_from_float.py
@@ -0,0 +1,65 @@
+from dataclasses import dataclass
+
+from mypy.nodes import CallExpr, MemberExpr, NameExpr
+
+from refurb.error import Error
+
+
+@dataclass
+class ErrorInfo(Error):
+    """
+    When constructing a Fraction or Decimal using a float, don't use the
+    `from_float()` or `from_decimal()` class methods: Just use the more consice
+    `Fraction()` and `Decimal()` class constructors instead.
+
+    Bad:
+
+    ```
+    ratio = Fraction.from_float(1.2)
+    score = Decimal.from_float(98.0)
+    ```
+
+    Good:
+
+    ```
+    ratio = Fraction(1.2)
+    score = Decimal(98.0)
+    ```
+    """
+
+    name = "no-from-float"
+    code = 164
+    categories = ["decimal", "fractions", "readability"]
+
+
+KNOWN_FUNCS = {
+    "Decimal.from_float",
+    "Fraction.from_float",
+    "Fraction.from_decimal",
+}
+
+
+def check(node: CallExpr, errors: list[Error]) -> None:
+    match node:
+        case CallExpr(
+            callee=MemberExpr(
+                expr=NameExpr(
+                    fullname="_decimal.Decimal" | "fractions.Fraction",
+                    name=klass,
+                ),
+                name="from_float" | "from_decimal" as ctor,
+            ),
+            args=[_],
+        ):
+            func = f"{klass}.{ctor}"
+
+            if func not in KNOWN_FUNCS:
+                return
+
+            errors.append(
+                ErrorInfo(
+                    node.line,
+                    node.column,
+                    f"Replace `{func}(x)` with `{klass}(x)`",
+                )
+            )
