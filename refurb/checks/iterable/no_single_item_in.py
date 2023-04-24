9652ede11f3599d23ebfdb6a251c04fcce6c961b
dosisod <39638017+dosisod@users.noreply.github.com>
2023-04-23 21:30:05 -0700

    Add `no-single-item-in` check (#250)

---
+++ refurb/checks/iterable/no_single_item_in.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+
+from mypy.nodes import ComparisonExpr, ListExpr, TupleExpr
+
+from refurb.error import Error
+
+
+@dataclass
+class ErrorInfo(Error):
+    """
+    Don't use `in` to check against a single value, use `==` instead:
+
+    Bad:
+
+    ```
+    if name in ("bob",):
+        pass
+    ```
+
+    Good:
+
+    ```
+    if name == "bob":
+        pass
+    ```
+    """
+
+    name = "no-single-item-in"
+    code = 171
+    categories = ["iterable", "readability"]
+
+
+def check(node: ComparisonExpr, errors: list[Error]) -> None:
+    match node:
+        case ComparisonExpr(
+            operators=["in" | "not in" as oper],
+            operands=[_, ListExpr() | TupleExpr() as expr],
+        ) if len(expr.items) == 1:
+            new_oper = "==" if oper == "in" else "!="
+
+            if isinstance(expr, ListExpr):
+                msg = f"Replace `x {oper} [y]` with `x {new_oper} y`"
+            else:
+                msg = f"Replace `x {oper} (y,)` with `x {new_oper} y`"
+
+            errors.append(ErrorInfo.from_node(node, msg))
---
+++ test/data/err_171.py
@@ -0,0 +1,12 @@
+# these should match
+
+_ = 1 in (1,)
+_ = 1 in [1]  # noqa: FURB109
+_ = 1 not in (1,)
+_ = 1 not in [1]  # noqa: FURB109
+
+
+# these should not
+
+_ = 1 in (1, 2)
+_ = 1 in [1, 2]  # noqa: FURB109
