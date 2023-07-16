c6e37022efaf2910cdbe7eacc4000eccb94c7ac0
sobolevn <mail@sobolevn.me>
2023-07-16 14:12:45 +0300

    Warn `datetime.{utcnow,utcfromtimestamp}` usages

---
+++ refurb/checks/datetime/unreliable_utc_usage.py
@@ -0,0 +1,61 @@
+from dataclasses import dataclass
+from typing import Final
+
+from mypy.nodes import CallExpr, MemberExpr, NameExpr
+
+from refurb.error import Error
+from refurb.settings import Settings
+
+
+@dataclass
+class ErrorInfo(Error):
+    """
+    Because naive `datetime` objects are treated by many `datetime` methods
+    as local times, it is preferred to use aware datetimes to represent times
+    in UTC.
+
+    This check affects `datetime.utcnow` and `datetime.utcfromtimestamp`.
+
+    Bad:
+
+    ```
+    from datetime import datetime
+
+    now = datetime.utcnow()
+    past_date = datetime.utcfromtimestamp(some_timestamp)
+    ```
+
+    Good:
+
+    ```
+    from datetime import datetime, timezone
+
+    datetime.now(timezone.utc)
+    datetime.fromtimestamp(some_timestamp, tz=timezone.utc)
+    ```
+    """
+
+    name = "unreliable-utc-usage"
+    code = 176
+    categories = ("datetime",)
+
+
+_replacements: Final = {
+    "utcnow": "now(tz=timezone.utc)",
+    "utcfromtimestamp": "fromtimestamp(..., tz=timezone.utc)",
+}
+
+
+def check(node: CallExpr, errors: list[Error], settings: Settings) -> None:
+    match node:
+        case CallExpr(
+            callee=MemberExpr(
+                expr=NameExpr(fullname="datetime.datetime"),
+            ),
+        ) if node.callee.name in {"utcnow", "utcfromtimestamp"}:
+            replacement = _replacements[node.callee.name]
+            errors.append(
+                ErrorInfo.from_node(
+                    node, f"Replace `{node.callee.name}` with `{replacement}`"
+                )
+            )
---
+++ test/data/err_176.py
@@ -0,0 +1,9 @@
+from datetime import datetime, timezone
+
+# Should warn:
+start_date = datetime.utcnow()
+old_date = datetime.utcfromtimestamp(1)
+
+# Should not warn:
+start_date = datetime.now(tz=timezone.utc)
+old_date = datetime.fromtimestamp(1, tz=timezone.utc)
