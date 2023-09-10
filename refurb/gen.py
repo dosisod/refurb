cbcf3bf16b5453a2e38e4fe0039d11695f412cd3
dosisod <39638017+dosisod@users.noreply.github.com>
2023-09-09 22:18:28 -0700

    Bump packages

--- refurb/gen.py
+++ refurb/gen.py
@@ -53,7 +53,7 @@ def fzf(data: list[str] | None, args: list[str]) -> str:
         "FZF_DEFAULT_COMMAND": "find refurb -name '*.py' -not -path '*__*' 2> /dev/null || true",  # noqa: E501
     }
 
-    process = run(
+    process = run(  # noqa: PLW1510
         ["fzf", "--height=20", *args],  # noqa: S603, S607
         env=env,
         stdout=PIPE,
