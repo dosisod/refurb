ac56b62ada809aa6bd4f41af02d3905966058370
dosisod <39638017+dosisod@users.noreply.github.com>
2023-12-26 21:35:01 -0800

    Fix linter errors

--- test/conftest.py
+++ test/conftest.py
@@ -1,8 +1,11 @@
-from unittest.mock import patch
+from collections.abc import Generator
+from unittest.mock import Mock, patch
+
 import pytest
 
+
 @pytest.fixture(autouse=True)
-def fake_tty():
+def fake_tty() -> Generator[Mock, None, None]:
     # Pytest doesnt run in a TTY, so the new TTY detection code is causing a lot of color related
     # tests to fail. This hack makes it so color is always enabled, like it would in a normal TTY.
     with patch("sys.stdout.isatty") as p:
